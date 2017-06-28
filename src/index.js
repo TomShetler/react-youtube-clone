8518dd63bf3ded28e9fd425419db37483ba8359b
ThomasShetler <tshetler93@gmail.com>
2017-06-28 11:07:57 -0400

    Add VidList, VidListItem, and SearchBar components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+
+const API_KEY = 'AIzaSyCT1--H_YkAaTSp-4PPO-v0v72cpUXDS6o';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos});
+    });
+  }
+
+  render() {
+  return (
+    <div>
+      <SearchBar/>
+      <VideoList videos={this.state.videos}/>
+    </div>
+  );
+}
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
